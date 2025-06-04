29806ff1abb1eb3fea1b61f2bb527df0e1b99ae4
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-06-04 23:41:15 +0600

    caregory in card view added

--- src/Pages/BookCategory/BookCategory.jsx
+++ src/Pages/BookCategory/BookCategory.jsx
@@ -1,19 +1,27 @@
 
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import CategoryCard from './CategoryCard';
 
 const BookCategory = () => {
     const {name}=useParams()
     const allBooks=useLoaderData()
     
     const categoryBooks=allBooks.filter((book)=>book.category.toLowerCase() === name)
-    console.log(categoryBooks);
+    
     return (
         <div className='py-12 lg:py-24'>
             <h1 className='text-6xl font-bold text-center text-pink-600'>Welcome To {name} Categories</h1>
 
+        <div className='container mx-auto py-12 lg:py-24 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 justify-center items-center'>
+            {
+                categoryBooks.map((singleBook)=><CategoryCard
+                singleBook={singleBook}
+                key={singleBook._id}
+                ></CategoryCard>)
+            }
+        </div>
 
-git
         </div>
     );
 };
---
+++ src/Pages/BookCategory/CategoryCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const CategoryCard = ({singleBook}) => {
+    const {
+  title,
+  imageUrl,
+  quantity,
+  author,
+  category,
+//   description,
+  rating, 
+//   content  --->dynamic rating will be added later
+} = singleBook;
+
+    return (
+        <div className=''>
+            <img className='w-48 h-48' src={imageUrl} alt="" />
+            <h2>{title}</h2>
+            <h3>{author}</h3>
+            <h2>Category: {category}</h2>
+            <h5>Quantity : {quantity}</h5>
+            <h5>Rating : {rating}</h5> 
+            <Link className='btn btn-success font-bold text-black'>View Details</Link>
+        </div>
+    );
+};
+
+export default CategoryCard;
