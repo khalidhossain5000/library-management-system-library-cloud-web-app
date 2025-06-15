c25e21b875e53ddb42b6d7ee24b022f71c430839
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-06-15 12:20:49 +0600

    add book page design updated

--- src/Pages/AddBook/AddBook.jsx
+++ src/Pages/AddBook/AddBook.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-import bgImg from "../../assets/SliderImg/reading-img.jpg";
+// import bgImg from "../../assets/SliderImg/reading-img.jpg";
+// import bgImg from "../../assets/AddUpdateBg/stock-id.jpg";
+// import bgImg from "../../assets/AddUpdateBg/nisfd.jpg";
+// import bgImg from "../../assets/AddUpdateBg/night-natures.jpg";-->more prefrable
+// import bgImg from "../../assets/AddUpdateBg/another-bg.jpg";
+// import bgImg from "../../assets/AddUpdateBg/bg-i.jpg";
+// import bgImg from "../../assets/AddUpdateBg/lib-gp.png"; -->more perfect one
+import bgImg from "../../assets/AddUpdateBg/lib-img-ii.jpg";
+// import bgImg from "../../assets/AddUpdateBg/library=i.jpg";----> this also good
+// import bgImg from "../../assets/AddUpdateBg/mntain-campsite.jpg";
 import axios from "axios";
 import toast from "react-hot-toast";
 import useAuth from "../../Hooks/useAuth";
@@ -49,125 +58,125 @@ const AddBook = () => {
         style={{ backgroundImage: `url(${bgImg})` }}
         className="bg-cover bg-no-repeat py-36 bg-top"
       >
-        <h1 className="text-[#feedff] container mx-auto text-center text-4xl lg:text-6xl font-bold py-12">
+        <h1 className="text-cyan-500 container mx-auto text-center text-3xl md:text-4xl lg:text-6xl font-bold py-6 lg:py-12">
           Add A Book
         </h1>
-        <h3 className="text-[#feedff] text-center text-xl lg:text-3xl font-medium">
+        <h3 className="text-cyan-200 text-center text-xl lg:text-3xl font-medium">
           Fill Up The Form To Add A Book!
         </h3>
-        <section className="max-w-9/12 mx-auto form-full mt-6 lg:mt-0 p-5 lg:p-24 ">
+        <section className="lg:max-w-9/12 mx-auto form-full mt-6 lg:mt-0 p-5 lg:p-14">
           <form onSubmit={handleAddBook}>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <fieldset className="bg-white/10 backdrop-blur-sm fieldset rounded-box border border-pink-300 p-4">
-                <label className="label text-pink-100 text-xl lg:text-2xl pb-3">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 " >
+              <fieldset className=" backdrop-blur-md  fieldset rounded-box border border-cyan-300 p-4 " style={{boxShadow:'0 0 10px #00a9e8'}}>
+                <label className="label text-cyan-300 text-xl lg:text-2xl pb-3">
                   Book Title
                 </label>
                 <input
                   type="text"
                   name="title"
-                  className="input w-full shadow-2xl bg-white/10 backdrop-blur-sm text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
+                  className="input w-full shadow-2xl bg-[#31b8ea20] backdrop-blur-sm text-pink-100 border-2 border-cyan-300 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl focus:shadow-2xl focus:shadow-cyan-300"
                   placeholder="Enter Your Book Name"
                 />
               </fieldset>
-              <fieldset className="bg-white/10 backdrop-blur-sm fieldset rounded-box border border-pink-300 p-4">
-                <label className="label text-pink-100 text-xl lg:text-2xl pb-3">
+              <fieldset className=" backdrop-blur-md  fieldset rounded-box border border-cyan-300 p-4 " style={{boxShadow:'0 0 10px #00a9e8'}}>
+                <label className="label text-cyan-300 text-xl lg:text-2xl pb-3">
                   Book Image URL
                 </label>
                 <input
                   type="url"
                   name="imageUrl"
-                  className="input w-full shadow-2xl bg-white/10 backdrop-blur-sm text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
+                  className="input w-full shadow-2xl bg-[#31b8ea20] backdrop-blur-sm text-pink-100 border-2 border-cyan-300 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl focus:shadow-2xl focus:shadow-cyan-300"
                   placeholder="Image URL"
                 />
               </fieldset>
               {/* quantity */}
-              <fieldset className="bg-white/10 backdrop-blur-sm fieldset rounded-box border border-pink-300 p-4">
-                <label className="label text-pink-100 text-xl lg:text-2xl pb-3">
+              <fieldset className=" backdrop-blur-md  fieldset rounded-box border border-cyan-300 p-4 " style={{boxShadow:'0 0 10px #00a9e8'}}>
+                <label className="label text-cyan-300 text-xl lg:text-2xl pb-3">
                   Enter Book Quantity
                 </label>
                 <input
                   type="number"
                   name="quantity"
-                  className="input w-full shadow-2xl bg-white/10 backdrop-blur-sm text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
+                  className="input w-full shadow-2xl bg-[#31b8ea20] backdrop-blur-sm text-pink-100 border-2 border-cyan-300 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl focus:shadow-2xl focus:shadow-cyan-300"
                   placeholder="Max Members"
                 />
               </fieldset>
               {/* quantity */}
               {/* author name */}
-              <fieldset className="bg-white/10 backdrop-blur-sm fieldset rounded-box border border-pink-300 p-4">
-                <label className="label text-pink-100 text-xl lg:text-2xl pb-3">
+              <fieldset className=" backdrop-blur-md  fieldset rounded-box border border-cyan-300 p-4 " style={{boxShadow:'0 0 10px #00a9e8'}}>
+                <label className="label text-cyan-300 text-xl lg:text-2xl pb-3">
                   Author Name
                 </label>
                 <input
                   type="text"
                   name="author"
                   placeholder="Author Name"
-                  className="input w-full shadow-2xl bg-white/10 backdrop-blur-sm text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
+                  className="input w-full shadow-2xl bg-[#31b8ea20] backdrop-blur-sm text-pink-100 border-2 border-cyan-300 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl focus:shadow-2xl focus:shadow-cyan-300"
                 />
               </fieldset>
               {/* author name */}
 
               {/* Category */}
-              <fieldset className="bg-white/10 backdrop-blur-sm fieldset rounded-box border border-pink-300 p-4">
-                <label className="label text-pink-100 text-xl lg:text-2xl pb-3">
+              <fieldset className=" backdrop-blur-md  fieldset rounded-box border border-cyan-300 p-4 " style={{boxShadow:'0 0 10px #00a9e8'}}>
+                <label className="label text-cyan-300 text-xl lg:text-2xl pb-3">
                   Select Book Category
                 </label>
                 <select
                   name="category"
-                  className="w-full shadow-2xl bg-white/10 backdrop-blur-sm  text-black font-semibold border-2 border-pink-500 py-3 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
+                  className="text-white w-full shadow-2xl backdrop-blur-sm font-semibold border-2 border-cyan-500 py-3 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
                 >
                   {/* <option disabled={true}>Hobby Category</option> */}
                   <option value="" hidden>
                     Select Book Category
                   </option>
-                  <option>Computer & Programming</option>
-                  <option>Science-Fiction</option>
-                  <option>Sports & Games</option>
-                  <option>Self-Development & Meditation</option>
+                  <option className="bg-cyan-900 ">Computer & Programming</option>
+                  <option className="bg-cyan-900 ">Science-Fiction</option>
+                  <option className="bg-cyan-900 ">Sports & Games</option>
+                  <option className="bg-cyan-900 ">Self-Development & Meditation</option>
                 </select>
               </fieldset>
               {/* Category */}
 
               {/* descriptions */}
-              <fieldset className="bg-white/10 backdrop-blur-sm fieldset rounded-box border border-pink-300 p-4">
-                <label className="label text-pink-100 text-xl lg:text-2xl pb-3">
+              <fieldset className=" backdrop-blur-md  fieldset rounded-box border border-cyan-300 p-4 " style={{boxShadow:'0 0 10px #00a9e8'}}>
+                <label className="label text-cyan-300 text-xl lg:text-2xl pb-3">
                   Add Short Description
                 </label>
                 <textarea
                   name="description"
                   placeholder="Description"
-                  className="w-full textarea textarea-xs input shadow-2xl bg-white/10 backdrop-blur-sm text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 !text-xl"
+                  className="w-full textarea textarea-xs input shadow-2xl bg-[#31b8ea20] backdrop-blur-md text-cyan-100 border-2 border-cyan-300 py-7 placeholder:text-[17px] placeholder:text-white focus:shadow-2xl focus:shadow-cyan-300 !text-xl"
                 ></textarea>
               </fieldset>
               {/* Rating  */}
-              <fieldset className="bg-white/10 backdrop-blur-sm fieldset rounded-box border border-pink-300 p-4">
-                <label className="label text-pink-100 text-xl lg:text-2xl pb-3">
+              <fieldset className=" backdrop-blur-md  fieldset rounded-box border border-cyan-300 p-4 " style={{boxShadow:'0 0 10px #00a9e8'}}>
+                <label className="label text-cyan-300 text-xl lg:text-2xl pb-3">
                   Enter A Rating
                 </label>
                 <input
                   type="number"
                   max="5"
                   name="rating"
-                  className="input w-full shadow-2xl bg-white/10 backdrop-blur-sm text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl"
+                  className="input w-full shadow-2xl bg-[#31b8ea20] backdrop-blur-sm text-pink-100 border-2 border-cyan-300 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 text-xl focus:shadow-2xl focus:shadow-cyan-300"
                   placeholder="Enter a rating"
                 />
               </fieldset>
-              <fieldset className="bg-white/10 backdrop-blur-sm fieldset rounded-box border border-pink-300 p-4">
-                <label className="label text-pink-100 text-xl lg:text-2xl pb-3">
+              <fieldset className=" backdrop-blur-md  fieldset rounded-box border border-cyan-300 p-4 " style={{boxShadow:'0 0 10px #00a9e8'}}>
+                <label className="label text-cyan-300 text-xl lg:text-2xl pb-3">
                   Add Book Content
                 </label>
                 <textarea
                   name="content"
                   placeholder="Add Book Content"
-                  className="w-full textarea textarea-xs input shadow-2xl bg-white/10 backdrop-blur-sm text-pink-100 border-2 border-pink-500 py-7 placeholder:text-[17px] placeholder:text-white focus:border-cyan-500 !text-xl"
+                  className="w-full textarea textarea-xs input shadow-2xl bg-[#31b8ea20] backdrop-blur-md text-cyan-100 border-2 border-cyan-300 py-7 placeholder:text-[17px] placeholder:text-white focus:shadow-2xl focus:shadow-cyan-300 !text-xl"
                 ></textarea>
               </fieldset>
             </div>
 
             <input
               type="submit"
-              className="border-3 border-cyan-600 w-full bg-white/10 backdrop-blur-sm p-6 text-2xl font-bold cursor-pointer text-pink-100 rounded-xl mt-4 hover:bg-gradient-to-tl hover:from-[#07233c] hover:via-[#1cc0de] hover:to-[#020611] transition duration-500"
-              value="Create Group"
+              className="cursor-pointer w-full bg-gradient-to-b from-[#0076ff] via-[#00bfff] to-[#0083ff]  p-8 border-cyan-300 text-xl text-pink-100 rounded-xl mt-4 hover:bg-gradient-to-br hover:from-[#001d16] hover:via-[#00ad82] hover:to-[#1d717e] transition duration-300 hover:scale-105 ease-in-out hover:animate-pulse"
+              value="Add Book"
             />
           </form>
         </section>
